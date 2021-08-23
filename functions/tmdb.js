c1c9705bc4c0018155c713753331105a61cd28ec
Darie Dorlus <tdorluss@gmail.com>
2021-08-23 15:27:12 -0400

    getMovies by genres

--- functions/tmdb.js
+++ functions/tmdb.js
@@ -8,26 +8,39 @@ const apikey = `7432733ca0b97edebd21bdd62d35ddd5`
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-const getMovies = (startDate, endDate, page) => {
+const getMovies = (startDate, endDate, page, genre) => {
   // const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apikey}&with_genres=${genre}&primary_release_date.gte=${startDate}&primary_release_date.lte=${endDate}&sort_by=primary_release_date.desc&with_original_language=en`;
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apikey}&sort_by=primary_release_date.desc&with_original_language=en&primary_release_date.gte=${startDate}&primary_release_date.lte=${endDate}&page=${page}`; // &page=10
+  //const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apikey}&sort_by=primary_release_date.desc&with_original_language=en&primary_release_date.gte=${startDate}&primary_release_date.lte=${endDate}&page=${page}`; // &page=10
+  let total_pages  = 1000000;
+  
+  while(page < total_pages)
+  {
+    setTimeout(()=> 0, 500)
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apikey}&sort_by=popularity.desc&with_original_language=en&primary_release_date.gte=${startDate}&primary_release_date.lte=${endDate}&page=${page}&with_genres=3${genre}`; // &page=10
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        
+        // connect to db
+        total_pages = data["total_pages"]
+        page++;
+        
+        admin.initializeApp({
+          credential: admin.credential.cert(creds),
+        })
+        const db = admin.firestore()
+        // now we can loop through each movie and add it to our movies collection
+        data.results.forEach(movie => {
+          // console.log('Adding movie ' + movie.title)
+          db.collection('movies-2').add(movie)
+        })
+      })
 
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      // connect to db
-      admin.initializeApp({
-        credential: admin.credential.cert(creds),
-      })
-      const db = admin.firestore()
-      // now we can loop through each movie and add it to our movies collection
-      data.results.forEach(movie => {
-        // console.log('Adding movie ' + movie.title)
-        db.collection('movies').add(movie)
-      })
-    })
+  }
+
 }
+
 
 let PageToDownload = process.argv[2];
 console.log("Fetching page",PageToDownload)
